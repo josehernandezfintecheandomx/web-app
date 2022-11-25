2fd2f55d946d852c425cb36e354665d33cbfa97e
Vic Romero <victor.romero@fintecheando.mx>
2022-11-25 00:26:45 -0600

    Fix the breadcrumb url generation

--- src/app/core/shell/breadcrumb/breadcrumb.component.ts
+++ src/app/core/shell/breadcrumb/breadcrumb.component.ts
@@ -119,8 +119,6 @@ export class BreadcrumbComponent implements OnInit, AfterViewInit {
               const routeData: Data = route.snapshot.data;
               if (routeData.breadcrumb === 'Clients') {
                 breadcrumbLabel = routeData.clientViewData.displayName;
-                var replaceGeneral = /\/general/gi;
-                currentUrl = url.replace(replaceGeneral, ""); 
                 currentUrl += `/general`;
               } else if (routeData.breadcrumb === 'Groups') {
                 breadcrumbLabel = routeData.groupViewData.name;
